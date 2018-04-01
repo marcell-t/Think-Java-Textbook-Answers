02938d1e61a98910022a50f31691416b2a493006
Marcell <marcelltothszabo@gmail.com>
2018-03-31 22:23:07 -0600

    Delete Exercise1-2.java

--- Exercise1-2.java
+++
@@ -1,8 +0,0 @@
-public class Hello {
-  // This is a comment
-  public static void main(String[] args){
-    System.out.println("Hello, World!");
-    System.out.println("How are you?");
-  }
-  
-}
