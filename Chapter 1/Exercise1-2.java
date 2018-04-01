05ad56c83455c68846e67d9b564cb4b1950b64a5
Marcell <marcelltothszabo@gmail.com>
2018-03-31 22:22:43 -0600

    Add files via upload

---
+++ Chapter 1/Exercise1-2.java
@@ -0,0 +1,8 @@
+public class Hello {
+  // This is a comment
+  public static void main(String[] args){
+    System.out.println("Hello, World!");
+    System.out.println("How are you?");
+  }
+  
+}
