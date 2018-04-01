5ddac229addb34dbc16724ebaf30c9053629763a
Marcell <marcelltothszabo@gmail.com>
2018-03-31 22:20:11 -0600

    Add files via upload

---
+++ Exercise1-1.java
@@ -0,0 +1,15 @@
+// Q: In computer jargon, what's the difference between a statement and a comment?
+
+// A: A statement is an action to be carried out, whereas a comment is an explanation left by the programmer.
+
+// Q: What does it mean to say that a program is portable? 
+
+// A: If a program is portable then it is able to function on multiple operating systems
+
+//Q: In common English, what does the word compile mean?
+
+//A: Compiling code means to turn it from code in a programming language to code that can actually used by a processor.
+
+//Q: What is an executable? 
+
+//A: Executing a program asks it to perform the task of whatever instructions it has been given
